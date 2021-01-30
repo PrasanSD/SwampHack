b330c099cfa0bbd457c60d2b14ff4cd2d29b2e3a
r-clarke <r.clarke@ufl.edu>
2021-01-29 23:13:14 -0500

    New pages file and load/new Project

--- project-partner-finder/src/App.js
+++ project-partner-finder/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here is a test
         </p>
         <a
           className="App-link"
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+
