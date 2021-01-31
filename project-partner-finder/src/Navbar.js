1bd79cd36adffcc90a64025746e68d28ec7032ae
Prasan Dhareshwar <prasandhareshwar@gmai.com>
2021-01-31 01:30:05 -0500

    Profile button

--- project-partner-finder/src/Navbar.js
+++ project-partner-finder/src/Navbar.js
@@ -6,21 +6,27 @@ export default function NavbarComponent() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>
-          LastHour
-        </Navbar.Brand>
+        <Navbar.Brand>LastHour</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/browse">Browse</Nav.Link>
-          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/browse">
+            Browse
+          </Nav.Link>
+          <Nav.Link as={Link} to="/projects">
+            Projects
+          </Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
         </Form>
-        <Nav.Link as={Link} to="/user">
-          Profile
-        </Nav.Link>
+        <Nav>
+          <Nav.Link as={Link} to="/user">
+            Profile
+          </Nav.Link>
+        </Nav>
       </Navbar>
     </>
   );
